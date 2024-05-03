9087bea5c610f770b34a1ea48a527b41660da972
Mohit Kumar Jaiswal <mohitkrjswl@gmail.com>
2024-05-04 01:07:20 +0530

    !st commit set up chat app and creating endpoint

---
+++ Backend/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+export const singup = (req, res) => {
+  console.log("signupUser");
+}
+export const login = (req, res) => {
+  res.send('Login User')
+  console.log("loginUser");
+}
+export const logout = (req, res) => {
+  console.log("logoutUser");
+}
